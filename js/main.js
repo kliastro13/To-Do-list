e679fabe2ed6c12b04b8c4df49db4888ee189af6
kliastro13 <it.goncharov@gmail.com>
2022-06-17 23:11:30 +0300

    Added branching to function "addNewElement". Added EventListener.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,19 @@
 function addNewElement() {
-  const element = new ListElement(
-    getElNumber(),
-    getCurrDate(),
-    getinputValue()
-  );
-
-  element.validate();
-}
-
-//function addNewElement() {}
-/* function newElement() {
-  const tableRow = document.createElement("tr");
   const inputValue = document.getElementById("task-input").value;
-
-  tableRow.innerHTML = `<th scope="row">1</th><td>13.06.22</td><td>${inputValue}</td><td><img src="img/x-octagon.svg" /></td>`;
-
-  if (!inputValue) {
-    alert("You must write something!");
+  if (!validate(inputValue)) {
+    return;
   } else {
-    document.getElementById("task-list").appendChild(tableRow);
+    const element = new ListElement(
+      getElNumber(),
+      getCurrDate(),
+      getinputValue()
+    );
+
+    element.inject();
+    const td = document.getElementById(element.number);
+    console.log(td);
+    td.addEventListener("click", function () {
+      delElement(element.number);
+    });
   }
-  document.getElementById("task-input").value = "";
-} */
+}
