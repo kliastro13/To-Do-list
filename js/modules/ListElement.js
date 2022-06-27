c2a6ebdde46e2733f030c2759a04161e63085a2c
kliastro13 <it.goncharov@gmail.com>
2022-06-27 22:08:24 +0300

    Added new classes. Customised bootstrap.

--- js/modules/ListElement.js
+++ js/modules/ListElement.js
@@ -7,11 +7,12 @@ class ListElement {
 
   inject(deleteTaskHandler) {
     const taskListId = "task-list";
+    const shortValue = this.value.substring(0, 50) + "...";
     const tableRow = document.createElement("tr");
     tableRow.setAttribute("id", this.id);
     tableRow.innerHTML = `<th scope="row">${this.id}</th><td>${this.date}</td>`;
-    tableRow.innerHTML += `<td>${this.value}</td>`;
-    tableRow.innerHTML += `<td class="del-btn"><i class="bi bi-trash" id="${this.id}-del-btn"></td>`;
+    tableRow.innerHTML += `<td title="${this.value}">${shortValue}</td>`;
+    tableRow.innerHTML += `<td class="delete-btn-container"><i class="bi bi-trash delete-btn" id="${this.id}-del-btn"></td>`;
     document.getElementById(taskListId).appendChild(tableRow);
 
     const deleteBtn = document.getElementById(`${this.id}-del-btn`);
