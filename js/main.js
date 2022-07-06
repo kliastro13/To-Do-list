6a73532183bb0915d609d899c1085d330b1cd14c
kliastro13 <it.goncharov@gmail.com>
2022-07-06 21:27:10 +0300

    Updated taskItemMaxLength

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import { addTaskItem, init } from "./modules/eventHandlers";
 
-const taskItemMaxLength = 20;
+const taskItemMaxLength = 100;
 
 window.addTaskItem = addTaskItem;
 window.addEventListener("load", init);
