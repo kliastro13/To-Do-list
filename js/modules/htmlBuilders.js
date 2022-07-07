f2d131253413ad8c4bd6810bf0646770512840a9
kliastro13 <it.goncharov@gmail.com>
2022-07-07 22:12:04 +0300

    Refactoring.

--- js/modules/htmlBuilders.js
+++ js/modules/htmlBuilders.js
@@ -1,29 +1,20 @@
-export function buildTaskItemHtml(element) {
+export function buildTaskItemHtml(id, value) {
   return `<div class="input-group input-group-sm">
-    <input type="text" class="form-control" value="${element.value}" id="${element.id}-edit-input">
-    <button type="button" class="btn btn-danger text-light ms-1" id="${element.id}-save-btn">
+    <input type="text" class="form-control" value="${value}" id="${id}-edit-input">
+    <button type="button" class="btn btn-danger text-light ms-1" id="${id}-save-btn">
       Save
     </button>
-    <button type="button" class="btn btn-danger text-light ms-1" id="${element.id}-cancel-btn">
+    <button type="button" class="btn btn-danger text-light ms-1" id="${id}-cancel-btn">
       Cancel
     </button>
   </div>`;
 }
 
-export function buildBtn(id) {
+export function buildActionsBtnHtml(id) {
   return `<button class="btn btn-outline-danger btn-sm" id="${id}-edit-btn">
             <i class="bi bi-pencil"></i>
           </button>
           <button class="btn btn-outline-danger btn-sm" id="${id}-del-btn">
             <i class="bi bi-trash"></i>
           </button>`;
-}
-
-{
-  /* <button class="btn btn-outline-danger btn-sm" id="${this.id}-edit-btn">
-<i class="bi bi-pencil"></i>
-</button>
-<button class="btn btn-outline-danger btn-sm" id="${this.id}-del-btn">
-<i class="bi bi-trash"></i>
-</button> */
 }
